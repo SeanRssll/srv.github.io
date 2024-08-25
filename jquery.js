3003575620dc67cca36b9a7c9dd06730c682079b
Vargas Sean <sdvargas@diwamail.com>
2024-08-26 05:48:59 +0800

    VARGAS UPDATE 08/26/24

--- jquery.js
+++ jquery.js
@@ -1,62 +1,55 @@
-  	new Vue({
-	    el: '#app',
-	    vuetify: new Vuetify(),
-	    data: {
-	        title: 'Portfolio',
-
-	        win:window.innerWidth,
-	        colors: [
-	          'indigo',
-	          'warning',
-	          'pink darken-2',
-	          'red lighten-1',
-	          'deep-purple accent-4',
-	        ],
-	        slides: [
-	          'First',
-	          'Second',
-	          'Third',
-	          'Fourth',
-	          'Fifth',
-	        ],
-
-	    }
-	})
-
-
-
 	$(document).ready(function(){
-	// function start
-	$("#aboutbtn").on("click",function(){
-		$(window).scrollTop("503");
-	});
-	$("#projectbtn").on("click",function(){
-		$(window).scrollTop("1596");
-	});
-	// function end
-	// animation start
-	$(".fadein").fadeIn(2000);
-	$(".showitems").show(3000);
-	$(window).scroll(function() {
-	  if ($(window).scrollTop() >=50) {
-		  $(".aboutMe").show(2000);
-	  }if ($(window).scrollTop() >=400) {
-	  	$(".skillTitle").fadeIn(2000);
-	  	$(".skillsTitle1").delay(200).show(200);
-	  	$(".skillsTitle2").delay(400).show(200);
-	  	$(".skillsTitle3").delay(600).show(200);
-	  	$(".skillsTitle4").delay(800).show(200);
-	  	$(".skillsTitle5").delay(1000).show(200);
-	  	$(".skillsTitle6").delay(1200).show(200);
-	  	$(".skillsTitle7").delay(1400).show(200);
-	  	$(".skillsTitle8").delay(1600).show(200);
-	  	$(".skillsTitle9").delay(1800).show(200);
-	  	$(".skillsTitle10").delay(2000).show(200);
-	  	$(".skillsTitle11").delay(2200).show(200);
-	  }if ($(window).scrollTop() >= 1200) {
-	  	$(".myprojects").fadeIn(2000);
-	  }
-	});
+		$(".skill-item").hover(function(params) {
+			html_td = "";
+			$(this).find(".img-prop").hide();
+			$(this).find(".level-prop").show();
+			$(this).find(".tr-data").attr("max_level");
+			levelcount = $(this).find(".tr-data").data("max");
+			for (let index = 0; index < levelcount; index++) {
+				dynamic_class = "col-count-"+index;
+				html_td +='<td class="column-style '+dynamic_class+'">&nbsp</td>';
+			}
+			$(this).find(".tr-data").append(html_td);
+			level = $(this).find(".tr-data").data("level");
+			for (let index = 0; index < level; index++) {
+				setTimeout(() => {
+					$(this).find(".col-count-" + index).css({
+						"background-color": "rgb(55, 255, 0)",
+						"transition": "background-color 0.1s ease"
+					});
+					count = index + 1;
+					$(this).find(".counter").html("<span class='count'>"+count+"<span>");
+				}, 100 * index);
+			}
+		},function () {
+			$(this).find(".count").remove();
+			$(this).find(".tr-data .column-style").remove();
+			$(this).find(".img-prop").show();
+			$(this).find(".level-prop").hide();
+		})
+		$(".fadein").fadeIn(2000);
+		$(".showitems").show(3000);
+		$(window).scroll(function() {
+		if ($(window).scrollTop() >=50) {
+			$(".aboutMe").show(2000);
+		}if ($(window).scrollTop() >=400) {
+			$(".skillTitle").fadeIn(2000);
+			$(".skillsTitle1").delay(200).show(200);
+			$(".skillsTitle2").delay(400).show(200);
+			$(".skillsTitle3").delay(600).show(200);
+			$(".skillsTitle4").delay(800).show(200);
+			$(".skillsTitle5").delay(1000).show(200);
+			$(".skillsTitle6").delay(1200).show(200);
+			$(".skillsTitle7").delay(1400).show(200);
+			$(".skillsTitle8").delay(1600).show(200);
+			$(".skillsTitle9").delay(1800).show(200);
+			$(".skillsTitle10").delay(2000).show(200);
+			$(".skillsTitle11").delay(2200).show(200);
+			$(".skillsTitle12").delay(2400).show(200);
+		}if ($(window).scrollTop() >= 1200) {
+			$(".mycert").fadeIn(200);
+		}
+		});
 	// animation end
 	// css-class start
 		if ($(window).width() <= 412) {
@@ -66,7 +59,7 @@
 			$(".aboutMeContentTitle").removeClass("text-h3").addClass("text-h4");
 			$(".avatarSize").width("60vw").height("60vw");
 			$(".skilltitle").removeClass("text-h3").addClass("text-h4").css("text-align","center");
-			$(".myprojects").removeClass("text-h3").addClass("text-h4").css("text-align","center");
+			$(".mycert").removeClass("text-h3").addClass("text-h4").css("text-align","center");
 			
 		}else{
 			$(".introContent").addClass("text-h6").removeClass("text-h7");
@@ -75,7 +68,7 @@
 			$(".aboutMeContentTitle").addClass("text-h3").removeClass("text-h4");
 			$(".avatarSize").width("35vw").height("35vw");
 			$(".skilltitle").addClass("text-h3").removeClass("text-h4").css("text-align","center");
-			$(".myprojects").addClass("text-h3").removeClass("text-h4").css("text-align","center");
+			$(".mycert").addClass("text-h3").removeClass("text-h4").css("text-align","center");
 			
 		}
 		// css-class end
---
+++ vue_script.js
@@ -0,0 +1,40 @@
+	
+	new Vue({
+	    el: '#app',
+	    vuetify: new Vuetify(),
+	    data: {
+			images: [
+				{ src: '../image/certificates/Suiterus.jpg', alt: 'Image 1'},
+				{ src: '../image/certificates/Condor.jpg', alt: 'Image 2'},
+				{ src: '../image/certificates/Pcworx.jpg', alt: 'Image 3'},
+				{ src: '../image/certificates/Waltermart.jpg', alt: 'Image 4'},
+				{ src: '../image/certificates/UCC.jpg', alt: 'Image 5'},
+				{ src: '../image/certificates/FEU.jpg', alt: 'Image 6'}
+				// Add more images as needed
+			  ],
+            skillset:[
+                { src: 'image/skills/vuetify.png', title: 'VUETIFY', class:"skillsTitle1",level:"6",max_level:"10"},
+                { src: 'image/skills/boostrap.png', title: 'BOOTSTRAP', class:"skillsTitle2",level:"8",max_level:"10"},
+                { src: 'image/skills/vue.png', title: 'VUE JS', class:"skillsTitle3",level:"6",max_level:"10"},
+                { src: 'image/skills/git.png', title: 'GIT', class:"skillsTitle4",level:"7",max_level:"10"},
+                { src: 'image/skills/laravel.png', title: 'LARAVEL', class:"skillsTitle5",level:"5",max_level:"10"},
+                { src: 'image/skills/jquery.png', title: 'JQUERY', class:"skillsTitle6",level:"6",max_level:"10"},
+                { src: 'image/skills/mysql.png', title: 'MYSQL', class:"skillsTitle7",level:"8",max_level:"10"},
+                { src: 'image/skills/html.png', title: 'HTML', class:"skillsTitle8",level:"8",max_level:"10"},
+                { src: 'image/skills/css.png', title: 'CSS', class:"skillsTitle9",level:"10",max_level:"10"},
+                { src: 'image/skills/axios.png', title: 'AXIOS', class:"skillsTitle10",level:"7",max_level:"10"},
+                { src: 'image/skills/php.png', title: 'PHP', class:"skillsTitle11",level:"9",max_level:"10"},
+                { src: 'image/skills/ajax.png', title: 'AJAX', class:"skillsTitle12",level:"9",max_level:"10"},
+
+
+            ]
+	    },methods: {
+			scrollToContainer(data) {
+				const element = document.getElementById(data);;
+				if (element) {
+				element.scrollIntoView({ behavior: 'smooth' });
+				}
+			}
+		}
+	})
+	
